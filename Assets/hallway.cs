26631674faa99ee05bdbb1c536f7f673e03f45a3
the noisette in your walls <andersonlevi12@outlook.com>
2025-09-22 19:20:53 +1000

    YOO I ADDED HALLWAYS WITH MY OWN CODE

---
+++ Assets/hallway.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class hallway : MonoBehaviour
+{
+    [SerializeField] private float x;
+    [SerializeField] private float y;
+    [SerializeField] private string room;
+
+    private Rigidbody2D body;
+    private Animator anim;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+	
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.CompareTag("Player"))
+		{
+			SceneManager.LoadScene(room);
+		}
+	}
+
+
+
+    private void Update()
+    {
+
+    }
+}
