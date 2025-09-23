4025aa23cd7be020f6c5a4cd76ae60968269dfb1
the noisette in your walls <andersonlevi12@outlook.com>
2025-09-24 00:09:49 +1000

    chapter select works with controllers

--- Assets/chapter select/chselect.cs
+++ Assets/chapter select/chselect.cs
@@ -2,14 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class chselect : MonoBehaviour
 {
-	[SerializeField] private string room;
-	
+    [SerializeField] private string room;
+    [SerializeField] private Button targetButton;
+
     public void ButtonPressed()
     {
         Debug.Log("Button pressed!");
-		SceneManager.LoadScene(room);
+        SceneManager.LoadScene(room);
+    }
+    void Update()
+    {
+        if (EventSystem.current.currentSelectedGameObject == targetButton.gameObject)
+        {
+            Debug.Log(targetButton.name + " is highlighted via navigation");
+            
+            if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.Z))
+            {
+                ButtonPressed();
+            }
+        }
     }
 }
