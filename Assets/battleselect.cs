6efa89ecf74fc9f75e90b68bac99ca3d52c711a9
the noisette in your walls <andersonlevi12@outlook.com>
2025-09-23 16:21:02 +1000

    made the selection work

---
+++ Assets/battleselect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class battleselect : MonoBehaviour
+{
+    //[SerializeField] private string room;
+    public GameObject battleBox;
+    public GameObject soul;
+
+    public void ButtonPressed()
+    {
+        Debug.Log("Button pressed!");
+        battleBox.SetActive(true);
+        soul.SetActive(true);
+        //SceneManager.LoadScene(room);
+    }
+}
