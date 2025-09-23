13bb661278d98f1509673283191d44ca78fb413f
the noisette in your walls <andersonlevi12@outlook.com>
2025-09-23 16:29:20 +1000

    changes

--- Assets/battleselect.cs
+++ Assets/battleselect.cs
@@ -8,12 +8,20 @@ public class battleselect : MonoBehaviour
     //[SerializeField] private string room;
     public GameObject battleBox;
     public GameObject soul;
+    public GameObject playerrow;
+    public GameObject self;
+    public GameObject speakertext;
+    public GameObject speakericon;
 
     public void ButtonPressed()
     {
         Debug.Log("Button pressed!");
         battleBox.SetActive(true);
+        playerrow.SetActive(false);
         soul.SetActive(true);
+        self.SetActive(true);
+        speakertext.SetActive(false);
+        speakericon.SetActive(false);
         //SceneManager.LoadScene(room);
     }
 }
