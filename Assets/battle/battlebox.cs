9eb15092e027b1ddb16a3c91a31bb2711576eb81
the noisette in your walls <andersonlevi12@outlook.com>
2025-09-23 15:30:00 +1000

    battle box

---
+++ Assets/battle/battlebox.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class battlebox : MonoBehaviour {
+
+	private Animator animator;
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (this.animator.GetCurrentAnimatorStateInfo(0).IsName("battlebox_create"))
+		{
+			Destroy(gameObject);
+		}
+	}
+}
