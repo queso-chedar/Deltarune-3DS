dfcb9b7a6eb94c5e7c8f243e852b53ae7b00324e
the noisette in your walls <andersonlevi12@outlook.com>
2025-09-24 22:19:57 +1000

    kris run button + kris room

--- Assets/NewKrisController.cs
+++ Assets/NewKrisController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class NewKrisController : MonoBehaviour
 {
-	public float speed;
+	private float speed;
 	public Animator animator;
 	public Rigidbody2D rb;
 	public Vector2 moveInput;
@@ -12,7 +12,7 @@ public class NewKrisController : MonoBehaviour
 	// Use this for initialization
 	void Start()
 	{
-
+		speed = 1.2f;
 	}
 
 	// Update is called once per frame
@@ -22,6 +22,17 @@ public class NewKrisController : MonoBehaviour
 		float moveY = Input.GetAxisRaw("Vertical");
 		moveInput = new Vector2(moveX, moveY).normalized;
 		Vector2 circlePad = UnityEngine.N3DS.GamePad.CirclePad;
+
+		if (Input.GetKey(KeyCode.X) || Input.GetKey(KeyCode.B)){
+			if (speed <= 3){
+				speed += 0.2f;
+			}
+		}
+		if (!Input.GetKey(KeyCode.X) && !Input.GetKey(KeyCode.B)){
+			if (speed > 1.2){
+				speed -= 0.2f;
+			}
+		}
 
 		animator.SetFloat("Horizontal", moveX);
 		animator.SetFloat("Vertical", moveY);
