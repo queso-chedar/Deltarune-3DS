cc5e259ca55581c861aca4f05d811b84a1e32715
Ralcactus <andersonlevi12@outlook.com>
2025-09-29 14:44:05 +1000

    almost done the car cutscene

---
+++ Assets/cameracaropening.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameracaropening : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.MoveTowards(transform.position, new Vector3(100, transform.position.y, transform.position.z), Time.deltaTime * 1f);
+	}
+}
