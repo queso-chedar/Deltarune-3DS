f0ac9c5b9f8d195bd6356ba45277be39669a3ca0
Ralcactus <andersonlevi12@outlook.com>
2025-10-11 20:04:44 +1100

    more stuff for cooking

---
+++ Assets/CH3 minigames/cooking/shadow_guy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shadow_guy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.MoveTowards(transform.position, new Vector3(-5.68f, transform.position.y, transform.position.z), Time.deltaTime * .6f);
+	}
+}
