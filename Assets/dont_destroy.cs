12b2a2f73664b9ba12cfb1dd9b951b0ff388349c
Ralcactus <andersonlevi12@outlook.com>
2025-10-11 23:31:42 +1100

    buncha tv world rooms

---
+++ Assets/dont_destroy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dont_destroy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad(transform.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
