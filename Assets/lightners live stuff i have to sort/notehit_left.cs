0d01584e9764f48046d6ff502199292e7a953e6a
Ralcactus <andersonlevi12@outlook.com>
2025-10-11 13:12:56 +1100

    wip lightners live

---
+++ Assets/lightners live stuff i have to sort/notehit_left.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class notehit_left : MonoBehaviour
+{
+
+	public Animator note_effect;
+
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+	
+	public void OnTriggerStay2D(Collider2D collision)
+	{
+		if (collision.CompareTag("leftnote"))
+		{
+			if (UnityEngine.N3DS.GamePad.GetButtonHold(N3dsButton.A) || Input.GetKeyDown(KeyCode.Z))
+			{
+				Debug.Log("note hit!");
+				Destroy(collision.gameObject);
+
+
+				if (note_effect.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f && !note_effect.IsInTransition(0))
+				{
+					note_effect.Play("effect", 0, 0f);
+				}
+			}
+		}
+	}
+}
---
+++ Assets/lightners live stuff i have to sort/notemove.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class notemove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate(Vector3.down * 1 * Time.deltaTime);
+	}
+}
